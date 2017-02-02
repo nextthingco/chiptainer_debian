aedc7927b483c30786f2993fa0e7b56d160e5048
Alex Kaplan <kaplan2539@gmail.com>
2017-02-01 16:09:21 -0800

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM armv7/armhf-debian:jessie
+
+ADD qemu-arm-static.tar.gz /
+
+RUN apt-get update && apt-get -y install file
---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+QEMU_STATIC_URL=http://kaplan2539.gitlab.io/baumeister/qemu-arm-static.tar.gz
+
+wget -c $QEMU_STATIC_URL
+
+docker build -t chiptainer/debian .
